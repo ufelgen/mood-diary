6b8504765502d7b04ec38bf135e96aa124354913
Ulrike Felgenhauer <115288914+ufelgen@users.noreply.github.com>
2022-12-18 21:46:57 +0100

    alter image container to not resize images (#3)

--- components/RandomImage.js
+++ components/RandomImage.js
@@ -1,23 +1,42 @@
 import styled from "styled-components";
 import Image from "next/image";
+import { ST } from "next/dist/shared/lib/utils";
 
 export default function RandomImage({ setImage }) {
   function getRandomNumber() {
-    return Math.floor(Math.random() * 99);
+    // adjust number according to number of images in collection
+    return Math.floor(Math.random() * 258);
   }
 
   const url =
     "https://source.unsplash.com/collection/1270951/" + getRandomNumber();
+  // adjust collection number
 
   return (
     <StyledImagePage>
-      <Image src={url} alt="cute animal" width={300} height={300} priority />
+      <StyledImageContainer>
+        <Image
+          src={url}
+          alt="cute animal"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />{" "}
+      </StyledImageContainer>
       <button onClick={() => setImage(false)}>back</button>
     </StyledImagePage>
   );
 }
 
+const StyledImageContainer = styled.div`
+  width: 300px;
+  height: 300px;
+  border-color: black;
+  position: relative;
+`;
+
 const StyledImagePage = styled.section`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -25,12 +44,14 @@ const StyledImagePage = styled.section`
   button {
     background-color: hotpink;
     color: white;
-    padding: 2rem;
+    padding: 1rem;
     border: 1px solid darkmagenta;
     border-radius: 5px;
     font-weight: bold;
     font-size: 20px;
     margin-top: 20px;
     width: 50%;
+    position: absolute;
+    bottom: -100px;
   }
 `;
