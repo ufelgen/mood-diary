9000c618b51c581857df9ed2cc7ad43004034754
Ulrike Felgenhauer <115288914+ufelgen@users.noreply.github.com>
2023-03-05 15:43:20 +0100

    bugfix/advice-firefox (#20)
    * clicking advice button several times does always return same item. unsuccessful in finding out why
    
    * add a random string to advice api fetch request
    
    * remove unused code

--- pages/advice/index.js
+++ pages/advice/index.js
@@ -3,13 +3,16 @@ import { useState } from "react";
 import Footer from "../../components/Footer";
 import RandomButton from "../../components/RandomImageButton";
 import RandomAdvice from "../../components/RandomAdvice";
+import { generateRandomString } from "../../helpers/generateRandomString";
 
 export default function Advice() {
   const [advice, setAdvice] = useState("");
+  const url =
+    "https://api.adviceslip.com/advice?random=" + generateRandomString(12);
 
   async function fetchAdvice() {
     try {
-      const response = await fetch("https://api.adviceslip.com/advice");
+      const response = await fetch(url);
       const data = await response.json();
       setAdvice(data.slip);
     } catch (error) {
